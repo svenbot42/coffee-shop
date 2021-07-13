2d97229ea0e103f7bb5b0b9287f0e58fad85b3c9
Steve Fagerhaug <sven@isnowhere.org>
2021-07-13 18:49:56 -0400

    add blog post page

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './BlogPost.module.css';
-export default function BlogPost({ title, date, excerpt }) {
+import { Link } from 'gatsby';
+
+export default function BlogPost({ title, date, excerpt, slug }) {
     return (
         <article className={styles.blog}>
-            <h2>{title}</h2>
+            <h2><Link to={slug}>{title}</Link></h2>
             <h3>{date}</h3>
             <p>{excerpt}</p>
         </article>
